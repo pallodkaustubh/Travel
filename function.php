92b35d2fc9e7cdf13dfa68958b7a90f025c1e44a
Kaustubh Pallod <127481844+pallodkaustubh@users.noreply.github.com>
2024-03-26 15:29:33 +0530

    Update function.php

--- function.php
+++ function.php
@@ -1,23 +1,38 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title></title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title></title>
 </head>
+
 <body>
 <?php
-function makeconnection() {
-    // Replace "your_password_here" with your actual database password
-    $password = "your_password_here";
-    $cn = mysqli_connect("localhost", "root", $password, "travel");
-    if (!$cn) {
-        die("Failed to connect to MySQL: " . mysqli_connect_error());
-    }
-    return $cn;
-}
+function makeconnection()
+{
+     
+  $cn = mysqli_init();
 
-// Attempt to make database connection
-$cn = makeconnection();
+    mysqli_ssl_set($cn,NULL,NULL, "DigiCertGlobalRootG2.crt.pem", NULL, NULL);
+
+// Establish the connection
+    mysqli_real_connect($cn, $host, $username, $password, $db_name, 3306, NULL, MYSQLI_CLIENT_SSL);
+
+//	$cn=mysqli_connect("localhost","root","","travel");
+
+	if(mysqli_connect_errno())
+	{
+		echo "failed to connect to mysqli:".mysqli_connect_error();
+	}
+	return $cn;
+
+}
+//$cn=mysqli_connect("localhost","root","","travel");
+
+$host = 'travel1.mysql.database.azure.com';
+     $username = 'traveladmin@travel1';
+     $password = 'server@123';
+     $db_name = 'travel';
+$cn=mysqli_connect($host, $username, $password, $db_name);
 ?>
 </body>
 </html>
